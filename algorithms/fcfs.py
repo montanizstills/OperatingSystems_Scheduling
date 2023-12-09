539d4c427e4cc93012d55e77926ab6e1675955ff
Montaniz.Stills <montanizstills@gmail.com>
2023-12-09 12:24:56 -0500

    stub  additional implementation;

--- algorithms/fcfs.py
+++ algorithms/fcfs.py
@@ -1,1 +1,15 @@
 ALGORITHM_NAME = "First-Come-First-Serve"
+
+test_processes = {
+    'A': (0, 4),
+    'B': (1, 9),
+    'C': (4, 2),
+    'D': ()
+}
+
+
+def fcfs_algorithm(
+        arrival_service_pairs: dict,
+        is_preemptive: bool
+):
+    pass
