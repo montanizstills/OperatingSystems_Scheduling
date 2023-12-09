eaf15ee6e34128749f624f4c7a39df7030ac1ff6
Montaniz.Stills <montanizstills@gmail.com>
2023-12-09 13:40:20 -0500

    i did these updates;

--- algorithms/fcfs.py
+++ algorithms/fcfs.py
@@ -4,7 +4,7 @@ test_processes = {
     'A': (0, 4),
     'B': (1, 9),
     'C': (4, 2),
-    'D': ()
+    'D': (6, 7)
 }
 
 
@@ -12,4 +12,17 @@ def fcfs_algorithm(
         arrival_service_pairs: dict,
         is_preemptive: bool
 ):
+    for process_name, (process_arrival, process_service) in test_processes.items():
+        exec(f"{process_name.lower()}_execution_time = {process_service}")
+        exec(f"{process_name.lower()}_arrival_time = {process_arrival}")
+
+        print(a_execution_time)
+
+    average_turn_around_time = 0  # GOAL
+    # wait time for each process # GOAL
+
     pass
+
+
+if __name__ == "__main__":
+    fcfs_algorithm(None, None)
--- algorithms/rr.py
+++ algorithms/rr.py
@@ -1,1 +1,28 @@
+from collections import deque
 ALGORITHM_NAME = "RoundRobin"
+
+arrivals = {0: "A", 3: "B", 6: "C", 8: "D"}
+services = [10,2,8,7]
+n = len(arrivals)
+sumOfServiceTimes = 0
+#Setting time quantum slightly larger than avg service time
+quantum = (sum(services) /n) + 1
+rem_st = [0] * n
+# Copy the burst time into rt[]
+for i in range(n):
+    rem_st[i] = services[i]
+t = 0
+wt = [0] * n
+queue = deque()
+done = False
+while (not done):
+    for i in range(sum(services) + 1):
+        if i in arrivals:
+            queue.enqueue(arrivals[i])
+
+
+
+
+            
+
+
