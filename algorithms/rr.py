835edaeb61bd1b365cda44432de2017ae354c15b
arjuns238 <92126319+arjuns238@users.noreply.github.com>
2023-12-12 16:46:39 -0500

    Update rr.py
    updated with data preprocessing

--- algorithms/rr.py
+++ algorithms/rr.py
@@ -10,7 +10,7 @@ when a process arrives:
     the process waits for its turn
     it runs for the quantum:
         1. a process finishes executing - dequeue next process
-        2. a process doesnt finish but exceeds its quantum - put process back in queue, dequeue next process.
+        2. a process doesn't finish but exceeds its quantum - put process back in queue, dequeue next process.
 '''
 import math
 from collections import deque
@@ -27,38 +27,101 @@ test_processes = {
     'D': (8, 7)
 }
 '''
-n = len(services)
 
-#waiting time
-wt = {}
+def run(data):
+    n = len(services)
 
-# Setting up latestLength to see how many seconds we have to loop to.
-latestArrival = max(arrivals.keys())
-longestService = 0
-for process in arrivals[latestArrival]:
-    longestService = max(longestService, services[process])
-latestLength = latestArrival + longestService
+    #waiting time
+    wt = {}
 
-# Setting time quantum slightly larger than avg service time
-quantum = 0
-avg_st = (sum(services.values()) / n)
-if math.ceil(avg_st) == avg_st:
-    quantum = avg_st + 1
-else:
-    quantum = math.ceil(avg_st)
-quantum = int(quantum)
-print("quantum = ", quantum)
+    # Setting up latestLength to see how many seconds we have to loop to.
+    latestArrival = max(arrivals.keys())
+    longestService = 0
+    for process in arrivals[latestArrival]:
+        longestService = max(longestService, services[process])
+    latestLength = latestArrival + longestService
 
-# Initializing remaining service time
-rt = {}
+    # Setting time quantum slightly larger than avg service time
+    quantum = 0
+    avg_st = (sum(services.values()) / n)
+    if math.ceil(avg_st) == avg_st:
+        quantum = avg_st + 1
+    else:
+        quantum = math.ceil(avg_st)
+    quantum = int(quantum)
+    print("quantum = ", quantum)
 
-# Initially remaining time is the same as service time
-for i in services.keys():
-    rt[i] = services[i]
+    # Initializing remaining service time
+    rt = {}
+
+    # Initially remaining time is the same as service time
+    for i in services.keys():
+        rt[i] = services[i]
+
+    queue = deque()
+    currProcess = ""
+
+    # Count to check whether process has executed up to quantum
+    currCount = -1
+
+    for i in range(max(sum(services.values()) + 1, latestLength)):
+        currCount += 1
+        # Increase the wait time for all the processes in queue
+        increaseWaitTime(queue, wt)
+
+        # If process arrives then begin servicing if no process currently running
+        # If there is an existing process, add this to queue
+        if i in arrivals:
+            if currProcess == "":
+                currProcess = arrivals[i][0]
+                for j in range(1, len(arrivals[i])):
+                    queue.append(arrivals[i][j])
+
+            else:
+                for j in range(len(arrivals[i])):
+                    queue.append(arrivals[i][j])
+
+        # If no process has arrived, then currProcess is set to ""
+        elif currProcess == "":
+            currCount = -1
+            continue
+
+        # If the process had been completed, then remove from processor and pop process from queue
+        if currProcess != "" and rt[currProcess] == 0:
+            if queue:
+                currProcess = queue.popleft()
+                currCount = 0
+            else:
+                currProcess = ""
+                currCount = -1
+
+            # If current count reaches quantum then put process back in queue
+        if currCount == quantum:
+            queue.append(currProcess)
+
+            # Pop the next process from queue
+            if queue:
+                currProcess = queue.popleft()
+                currCount = 0
+            else:
+                currProcess = ""
+                currCount = -1
+
+        # If a process is running, reduce remaining time to completion by 1 second
+        if currProcess != "":
+            rt[currProcess] -= 1
+
+    print("wait times = ", dict(sorted(wt.items())))
+
+    tt = calculateTurnaroundTime(services, wt)
+    print("turnaround time =", tt)
+
+    avg_tt = sum(tt.values()) / len(tt.values())
+    print("Average turnaround time  =", avg_tt)
 
 
 # Function to increase the wait times of the processes every second they are in the waiting queue.
-def increaseWaitTime(queue):
+def increaseWaitTime(queue, wt):
     if queue:
         for process in queue:
             wt[process] = wt.get(process, 0) + 1
@@ -72,64 +135,14 @@ def calculateTurnaroundTime(services, wt):
     return tt
 
 
-queue = deque()
-currProcess = ""
-
-# Count to check whether process has executed up to quantum
-currCount = -1
-
-for i in range(max(sum(services.values()) + 1, latestLength)):
-    currCount += 1
-    # Increase the wait time for all the processes in queue
-    increaseWaitTime(queue)
-    
-    # If process arrives then begin servicing if no process currently running
-    # If there is an existing process, add this to queue
-    if i in arrivals:
-        if currProcess == "":
-            currProcess = arrivals[i][0]
-            for j in range(1, len(arrivals[i])):
-                queue.append(arrivals[i][j])
-
-        else:
-            for j in range(len(arrivals[i])):
-                queue.append(arrivals[i][j])
-
-    # If no process has arrived, then currProcess is set to ""
-    elif currProcess == "":
-        currCount = -1
-        continue
-
-    # If the process had been completed, then remove from processor and pop process from queue
-    if currProcess != "" and rt[currProcess] == 0:
-        if queue:
-            currProcess = queue.popleft()
-            currCount = 0
-        else:
-            currProcess = ""
-            currCount = -1
-
-        # If current count reaches quantum then put process back in queue
-    if currCount == quantum:
-        queue.append(currProcess)
-
-        # Pop the next process from queue
-        if queue:
-            currProcess = queue.popleft()
-            currCount = 0
-        else:
-            currProcess = ""
-            currCount = -1
-
-    # If a process is running, reduce remaining time to completion by 1 second
-    if currProcess != "":
-        rt[currProcess] -= 1
-
-
-print("wait times = ", dict(sorted(wt.items())))
-
-tt = calculateTurnaroundTime(services, wt)
-print("turnaround time =", tt)
+if __name__ == "__main__":
+    data = {
+        'A': (0, 10),
+        'B': (3, 2),
+        'C': (6, 8),
+        'D': (8, 7)
+    }
+    run(data)
 
 
 
