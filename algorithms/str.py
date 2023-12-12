7d1f2803f8448a3550a9e0342067f7cd36e989c6
arjuns238 <92126319+arjuns238@users.noreply.github.com>
2023-12-12 16:43:45 -0500

    Update str.py
    Updated with data preprocessing

--- algorithms/str.py
+++ algorithms/str.py
@@ -1,4 +1,3 @@
-ALGORITHM_NAME = "ShortestTimeRemaining"
 
 '''
 processes have arrival times and service times.
@@ -18,9 +17,12 @@ when a process arrives:
         queue.sort_by_rt()
         update srt
 
+
+
 queue must be sorted after every insertion in descending order of their service times.
 '''
 import math
+ALGORITHM_NAME = "ShortestTimeRemaining"
 
 # Saving the value as a list because multiple processes can arrive at the same time
 arrivals = {0: ["A"], 3: ["B"], 6: ["C"], 8: ["D"]}
@@ -33,6 +35,20 @@ test_processes = {
     'D': (8, 7)
 }
 
+def convertData(data):
+    arrivals = {}
+    services = {}
+    for key in data:
+        services[key] = data[key][1]
+    for key in data:
+        arrival = data[key][0]
+        if arrival in arrivals:
+            arrivals[arrival] = arrivals[arrival].append(key)
+        else:
+            arrivals[arrival] = [key]
+    return arrivals, services
+
+
 class Process:
     def __init__(self, process_name, arrival_time, service_time, remaining_time):
         self.process_name = process_name
@@ -41,28 +57,10 @@ class Process:
         self.remaining_time = remaining_time
 
 
-n = len(services)
-
-# Waiting time
-wt = {}
-
-# Setting up latestLength to see how many seconds we have to loop to.
-latestArrival = max(arrivals.keys())
-longestService = 0
-for process in arrivals[latestArrival]:
-    longestService = max(longestService, services[process])
-latestLength = latestArrival + longestService
-
-# Initializing remaining service time
-rt = {}
-
-# Initially remaining time is the same as service time
-for i in services.keys():
-    rt[i] = services[i]
 
 
 # Function to increase the wait times of the processes every second they are in the waiting queue.
-def increaseWaitTime(queue):
+def increaseWaitTime(queue, wt):
     if queue:
         for process in queue:
             wt[process.process_name] = wt.get(process.process_name, 0) + 1
@@ -76,58 +74,91 @@ def calculateTurnaroundTime(services, wt):
     return tt
 
 
-queue = []
-currProcess = ""
+def run(data):
 
-for i in range(max(sum(services.values()) + 1, latestLength)):
-    print("time = ", i)
+    arrivals, services = convertData(data)
+    n = len(services)
+    # Waiting time
+    wt = {}
+
+    # Setting up latestLength to see how many seconds we have to loop to.
+    latestArrival = max(arrivals.keys())
+    longestService = 0
+    for process in arrivals[latestArrival]:
+        longestService = max(longestService, services[process])
+    latestLength = latestArrival + longestService
+
+    # Initializing remaining service time
+    rt = {}
+
+    # Initially remaining time is the same as service time
+    for i in services.keys():
+        rt[i] = services[i]
+
+    queue = []
+    currProcess = ""
+
+    for i in range(max(sum(services.values()) + 1, latestLength)):
+        print("time = ", i)
+
+        # Increase the wait time for all the processes in queue
+        increaseWaitTime(queue, wt)
+        # If process arrives then begin servicing if no process currently running
+        # If there is an existing process, add this to queue
+        if i in arrivals:
+            if currProcess != "":
+                queue.append(currProcess)
+                currProcess = ""
+            for j in range(len(arrivals[i])):
+                process_name = arrivals[i][j]
+                arrival, service = test_processes[process_name]
+                remaining = service
+                process = Process(process_name, arrival, service, remaining)
+                queue.append(process)
+            # Sorting the queue in decreasing order of their service times
+            queue.sort(key=lambda process: process.remaining_time, reverse=True)
+
+            if currProcess == "" and queue:
+                currProcess = queue.pop()
+
+        # If no process has arrived, then currProcess is set to ""
+        elif currProcess == "":
+            continue
+
+        # If the process had been completed, then remove from processor and pop process from queue
+        if currProcess != "" and currProcess.remaining_time == 0:
+            if queue:
+                currProcess = queue.pop()
+            else:
+                currProcess = ""
+        # If a process is running, reduce remaining time to completion by 1 second
+        if currProcess != "" and currProcess.remaining_time != 0:
+            currProcess.remaining_time -= 1
+
+        if currProcess:
+            print("Curr process:", currProcess.process_name)
+            print(wt)
+
+    print("wait times = ", dict(sorted(wt.items())))
+
+    tt = calculateTurnaroundTime(services, wt)
+    print("turnaround time =", tt)
+
+    avg_tt = sum(tt.values()) / len(tt.values())
+    print("Average turnaround time  =", avg_tt)
 
 
-    # Increase the wait time for all the processes in queue
-    increaseWaitTime(queue)
-    # If process arrives then begin servicing if no process currently running
-    # If there is an existing process, add this to queue
-    if i in arrivals:
-        if currProcess != "":
-            queue.append(currProcess)
-            currProcess = ""
-        for j in range(len(arrivals[i])):
-            process_name = arrivals[i][j]
-            arrival, service = test_processes[process_name]
-            remaining = service
-            process = Process(process_name, arrival, service, remaining)
-            queue.append(process)
-        # Sorting the queue in decreasing order of their service times
-        queue.sort(key=lambda process: process.remaining_time, reverse=True)
-
-        if currProcess == "" and queue:
-            currProcess = queue.pop()
-
-    # If no process has arrived, then currProcess is set to ""
-    elif currProcess == "":
-        continue
-
-    # If the process had been completed, then remove from processor and pop process from queue
-    if currProcess != "" and currProcess.remaining_time == 0:
-        if queue:
-            currProcess = queue.pop()
-        else:
-            currProcess = ""
-    # If a process is running, reduce remaining time to completion by 1 second
-    if currProcess != "" and currProcess.remaining_time != 0:
-        currProcess.remaining_time -= 1
-
-    if currProcess:
-        print("Curr process:", currProcess.process_name)
-        print(wt)
 
 
-print("wait times = ", dict(sorted(wt.items())))
 
-tt = calculateTurnaroundTime(services, wt)
-print("turnaround time =", tt)
-
-
+if __name__ == "__main__":
+    data = {
+        'A': (0, 10),
+        'B': (3, 2),
+        'C': (6, 8),
+        'D': (8, 7)
+    }
+    run(data)
 
 
 
