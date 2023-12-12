59e914891cbacdfcc65b643cc7f33aacfaf5c1e4
arjun <arjun.sriram238@gmail.com>
2023-12-12 18:40:17 -0500

    Added link to backend and added custom alg.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 
-from algorithms.spn import run
+from algorithms import spn, str, rr, hrrn, customAlg
 
 app = Flask(__name__)
 
@@ -8,7 +8,32 @@ app = Flask(__name__)
 @app.route("/spn", methods=["POST"])
 def do_spn():
     # print(request.get_json())
-    run(request.get_json())
+    spn.run(request.get_json())
+    return ""
+
+
+@app.route("/str", methods=["POST"])
+def do_str():
+    # print(request.get_json())
+    str.run(request.get_json())
+    return ""
+
+@app.route("/rr", methods=["POST"])
+def do_rr():
+    # print(request.get_json())
+    rr.run(request.get_json())
+    return ""
+
+@app.route("/hrrn", methods=["POST"])
+def do_hrrn():
+    # print(request.get_json())
+    hrrn.run(request.get_json())
+    return ""
+
+@app.route("/own", methods=["POST"])
+def do_own():
+    # print(request.get_json())
+    customAlg.run(request.get_json())
     return ""
 
 
